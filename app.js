f793cdef8ac1470f14c0f52f02d71079fb4e751f
Gabriel-Santos01 <gabriel.adv45@outlook.com>
2023-02-25 23:41:34 -0300

    feat: desktop responsibility

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const key = '?key6b8af869'
 const HgApi = `https://api.hgbrasil.com/finance${key}`
-
 function getCurrancy() {
   fetch(HgApi)
     .then(response => response.json())
-    .then(data => (renderAPIResult.textContent = JSON.stringify(data)))
+    .then(data => console.log(data))
     .catch(error => console.error(error))
 }
 
